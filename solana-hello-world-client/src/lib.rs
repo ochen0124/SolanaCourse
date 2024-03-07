977f22cff2682b0ce7bd9e60f0a02b768473d9c0
ochen0124 <ochen@bu.edu>
2024-03-06 18:49:54 -0700

    movie review backend solana program

--- solana-hello-world-client/src/lib.rs
+++ solana-hello-world-client/src/lib.rs
@@ -1,3 +1,5 @@
+pub mod instruction;
+use instruction::{MovieInstruction};
 use solana_program::{
     account_info::AccountInfo,
     entrypoint,
@@ -14,6 +16,28 @@ pub fn process_instruction(
     instruction_data: &[u8]
 ) -> ProgramResult{
     msg!("Hi, crypto world!");
+
+    let instruction = MovieInstruction::unpack(instruction_data)?;
+    
+    match instruction {
+        MovieInstruction::AddMovieReview { title, rating, description } => {
+            add_movie_review(program_id, accounts, title, rating, description)
+        }
+    }
+}
+
+pub fn add_movie_review(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    title: String,
+    rating: u8,
+    description: String
+) -> ProgramResult {
+
+    msg!("Adding movie review...");
+    msg!("Title: {}", title);
+    msg!("Rating: {}", rating);
+    msg!("Description: {}", description);
 
     Ok(())
 }
